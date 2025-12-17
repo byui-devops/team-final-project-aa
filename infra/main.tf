6d6c3a21194e90f59fcdf029819d71dbfa81bbad
angeloantoniou <ant22006@byui.edu>
2025-12-16 17:54:06 -0700

    i think this is it

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,25 @@
-provider "aws" {
+pprovider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "app" {
-  ami           = "ami-0c02fb55956c7d316"
+  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2
   instance_type = "t2.micro"
 
   user_data = <<-EOF
               #!/bin/bash
-              docker run -d -p 80:5000 ant22006/final-project
+              yum update -y
+              yum install -y docker
+              service docker start
+              usermod -a -G docker ec2-user
+              docker run -d -p 5000:5000 ant22006/final-project:latest
               EOF
+
+  tags = {
+    Name = "final-project-app"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.app.public_ip
 }
